3a438dab3b781eb5549f55bbccb91f120d85fabb
Jay Patel <jaypatel32157@gmail.com>
2021-10-10 13:40:07 +0530

    Hard but maja aya

---
+++ scheduling/assets/js/rr.js
@@ -0,0 +1,247 @@
+let processObject =Array();
+let chartData = [];
+let chartLabels = [];
+let processSchedule = [];
+let myChart;
+
+const deleteList = (id) =>{
+    let newObject = processObject.filter(process=>process.id != id)
+    processObject = [...newObject];
+    fillTable()
+}
+
+const appendValue = (event)=>{
+    event.preventDefault()
+    let AT = $('#arrival_time').val()
+    let BT = $('#burst_time').val()
+    
+    let form = $(".input-container")[0]
+    if(AT.match(/^[0-9]+$/) && BT.match(/^[0-9]+$/) ){
+        let l = processObject.length
+        processObject.push({
+            id:`P${l+1}`,
+            at:Number(AT),
+            bt:Number(BT),
+            st:0,
+            ct:0,
+            rt:0,
+            wt:0,
+            tat:0,
+            tembt:Number(BT)
+        })
+        form.reset()
+        fillTable()
+    }
+    else{
+        $('.toast').toast('show');
+        form.reset()
+    }
+}
+
+const fillTable = () =>{
+    let html =""
+    processObject.map((process,index)=>{
+        html += `<tr>
+                        <td scope="row" style="font-weight:bold">${process.id}</td>
+                        <td>${process.at}</td>
+                        <td>${process.bt}</td>
+                        <td>${process.st}</td>
+                        <td>${process.ct}</td>
+                        <td>${process.rt}</td>
+                        <td>${process.wt}</td>
+                        <td>${process.tat}</td>
+                        <td><button class="btn btn-danger p-1" style="position: inherit;" onclick="deleteList('${process.id}')"><i class="bi bi-archive"></i></button></td>
+                    </tr>`
+        })
+        $("#tableBody").empty()
+        $("#tableBody").append(html)
+}
+
+const play = (event) =>{
+    let QT = $('#time_quantum').val()
+    if(processObject.length == 0 || !QT.match(/^[0-9]+$/)){
+        $('.toast').toast('show');
+        return false;
+    }
+
+    event.preventDefault()
+    chartData = []
+    chartLabels = []
+    processSchedule = []
+    let avgTAT = 0
+    let avgWT=0
+    let avgRT=0
+
+    let canvas = document.getElementById('myChart');
+    ctx = document.getElementById('myChart').getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    if(myChart){
+        myChart.destroy();
+    }
+
+    processObject.sort((a, b) => {
+        return a.at - b.at;
+    });
+
+    const sumall = processObject.map(item => item.bt).reduce((prev, curr) => prev + curr, 0);
+
+    console.log(sumall)
+    
+    let steps =Number(QT);
+    let k = 0
+
+    let idArr = []
+    let ctArr = []
+
+    while(steps<sumall+Number(QT)){
+        
+        if(processObject[k].tembt !== 0){
+            idArr.push(processObject[k].id)
+            processObject[k].ct = steps
+            let neg = processObject[k].tembt - Number(QT)
+            
+            if(processObject[k].tembt - Number(QT) < 0){
+                processObject[k].ct += neg
+                processObject[k].tembt = 0
+                steps += neg
+                
+            }else{
+                
+                 processObject[k].tembt -= Number(QT)
+            }
+            if(processObject.length != k+1){
+                if(processObject[k+1].st == 0 && processObject[k+1].at <= steps){
+                    processObject[k+1].st = steps
+                }
+            }
+                ctArr.push(steps)
+                steps+=Number(QT)
+            
+        }     
+        
+        
+        console.log(processObject[k].id, processObject[k].st,processObject[k].ct,steps)
+        if(processObject.length == k+1){
+            k=0;
+        }else{
+            if(processObject[k+1].at <= steps-Number(QT)){
+                k++
+            }
+        }
+        
+        
+    }
+
+    
+
+    console.log(processObject)
+
+
+    for(let i=0;i<processObject.length;i++){
+        processObject[i].rt = processObject[i].st-processObject[i].at
+        processObject[i].tat = processObject[i].ct-processObject[i].at
+        processObject[i].wt = processObject[i].tat-processObject[i].bt
+        avgTAT += processObject[i].tat
+        avgWT += processObject[i].wt
+        avgRT += processObject[i].rt
+    }
+    processSchedule = [...processObject]
+    avgTAT = avgTAT/processObject.length
+    avgWT = avgWT/processObject.length
+    avgRT = avgRT/processObject.length
+
+
+
+    processObject.sort((a, b) => {
+        return Number(a.id.slice(1)) - Number(b.id.slice(1));
+    });
+
+    processObject.map(process=>{
+        chartData.push(process.tat)
+        chartLabels.push(process.id)
+    })
+
+    let html = `<h4>Average TAT: ${avgTAT.toFixed(2)}</h4><h4>Average WT: ${avgWT.toFixed(2)}</h4><h4>Average RT: ${avgRT.toFixed(2)}</h4>`
+
+    $("#ans").html("")
+    $("#ans").append(html)
+
+    fillTable()
+    processTable(idArr,ctArr)
+    makeGraph()
+    $('.title-cont').css({display:"block"})
+}
+
+const processTable = (idArr,ctArr) => {
+    let html =`
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">Start Time</th>`
+                    
+    for(let i =0;i<idArr.length;i++){
+        html += `<th scope="col">${idArr[i]}</th>`;
+    }
+
+    html += `   </tr>
+            </thead>
+            <tbody>
+                <tr><td>${processSchedule[0].st}</td>`;
+
+    for(let i =0;i<ctArr.length;i++){
+        html += `<td>${ctArr[i]}</td>`;
+    }
+
+    html +=    `</tr>
+            </tbody>
+        </table>
+    `
+    $("#process").html("")
+    $("#process").append(html)
+}
+
+const makeGraph = () =>{
+    var ctx = document.getElementById('myChart').getContext('2d');
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels:chartLabels,
+            datasets: [{
+                label:"TAT",
+                data: chartData,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                x: {
+                    stacked:true,
+                }
+            },
+        }
+    });
+
+}
